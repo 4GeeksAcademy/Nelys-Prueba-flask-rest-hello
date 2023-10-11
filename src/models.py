ae6ca72619e0be313fd53536bbeff79177649da6
NelysT <139475672+NelysT@users.noreply.github.com>
2023-10-11 20:47:31 +0000

    Modificando src/models.py - se agregan campos a la tabla

--- src/models.py
+++ src/models.py
@@ -3,17 +3,20 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.id
 
     def serialize(self):
         return {
             "id": self.id,
             "email": self.email,
+            "name" : self.name,
             # do not serialize the password, its a security breach
         }
